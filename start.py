5028b976ff65ab1f67ddbc62524379d79dc7c2cc
David C <dlccosio@gmail.com>
2020-04-25 22:56:44 -0400

    - Created the start

--- start.py
+++ start.py
@@ -1,1 +1,21 @@
-import tkinter
+import pickle
+
+while True:
+    print("********************")
+    print("*     Welcome      *")
+    print("*   To Our Game!   *")
+    print("********************")
+
+    print("Type 'Start' to start!")
+    print("Type 'Load' to load your save file!")
+
+    if (input().lower() == "start"):
+        print("started game")
+        break
+
+    elif (input().lower() == "load"):
+        print("loaded save")
+        break
+    
+    else: 
+        print("Fucking type something right you twat")
