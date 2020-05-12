50cb649992e27418712615ff5a891edcb0ef1dfa
ImaniB <idb7503@gmail.com>
2020-05-11 22:12:35 -0400

    help

--- start.py
+++ start.py
@@ -22,15 +22,6 @@ while True:
     else: 
         print("Fucking type something right you twat")
 
-def enemy_generator():
-<<<<<<< HEAD
-     
-
-import tkinter
-class createPlayer():
-    def playerChar(name, weapon, level, stats):
-       self.name = name 
-=======
+  def enemy_generator():
     if (playerLevel >= 1 && playerLevel <= 5):
-        lowEnemyStuff = {"Attack": }  
->>>>>>> e8bd751ce3437a61a7cce02708c652b2a7eae437
+        lowEnemyStuff = {"Attack": }
