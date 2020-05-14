3b4131e5a4572555ce6567e4935911aa09c8e3a2
David C <dlccosio@gmail.com>
2020-05-13 21:24:31 -0400

    -aaaaaaaaaaaaaaaa

--- ItemGenerator.py
+++ ItemGenerator.py
@@ -1,19 +1,19 @@
 import random
 def itemCreation(playerLevel:5):
 
+    def itemNameGenerator(weaponType):
+
     createdItem = {"name":None, "type":None, "itemHP":None, "itemATT":None, "itemDEF":None, "itemmATT":None, "itemmDEF":None}
 
     itemType = random.randint(1,10)
-
     if itemType in [1,2,3,4]:
-        createdItem["name"] = itemNameGenerator()
+        createdItem["name"] = itemNameGenerator(itemType)
         createdItem["itemHP"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemATT"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemDEF"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemmATT"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemmDEF"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         
-
 
 
 print(itemCreation(5))
