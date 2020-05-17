5640d85e9805aa562ddc37086ce2225ab48215ef
David C <dlccosio@gmail.com>
2020-05-16 21:35:26 -0400

    - made item naming/categories more intuitive(?)

--- ItemGenerator.py
+++ ItemGenerator.py
@@ -5,9 +5,10 @@ def itemCreation(playerLevel:5):
 
     createdItem = {"name":None, "type":None, "itemHP":None, "itemATT":None, "itemDEF":None, "itemmATT":None, "itemmDEF":None}
 
-    itemType = random.randint(1,10)
-    if itemType in [1,2,3,4]:
-        createdItem["name"] = itemNameGenerator(itemType)
+    typeList = ['Sword','Bow','Staff','Tome','Armor']
+    category = random.choice(typeList)
+    createdItem["type"] = category
+    createdItem["name"] = itemNameGenerator(category)
         createdItem["itemHP"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemATT"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
         createdItem["itemDEF"] = random.randint(0,3)*playerLevel + random.randint(-1*playerLevel,playerLevel)
