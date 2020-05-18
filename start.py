a759daf61c60be3a0d3c387994afc7c29d92ebad
David C <dlccosio@gmail.com>
2020-05-17 20:42:53 -0400

    - Changed 'fucking' to 'fricking'

--- start.py
+++ start.py
@@ -20,7 +20,7 @@ while True:
         break
     
     else: 
-        print("Fucking type something right you twat")
+        print("Fricking type something right you twat")
 
 def createPlayer():
     playerDictionary = {"name":input("Please input your name:") , "level":1 , "health":10 , "pdef":random.randint(0, 3) , "mdef":random.randint(0, 3) , "atk":random.randint(0, 3) , "matk":random.randint(0, 3) , "gold":0 , "weapon":"bare hands" , "armor":"commoner clothes"}
