1bed7cb061a057060d95bd6a325db448cdd06273
David C <dlccosio@gmail.com>
2020-05-17 21:59:11 -0400

    Made player dict more readable

--- start.py
+++ start.py
@@ -23,7 +23,17 @@ while True:
         print("Fricking type something right you twat")
 
 def createPlayer():
-    playerDictionary = {"name":input("Please input your name:") , "level":1 , "health":10 , "pdef":random.randint(0, 3) , "mdef":random.randint(0, 3) , "atk":random.randint(0, 3) , "matk":random.randint(0, 3) , "gold":0 , "weapon":"bare hands" , "armor":"commoner clothes" , "stageNum":1}
+    playerDictionary = {"name":input("Please input your name:") , 
+    "level":1 , 
+    "health":10 , 
+    "pdef":random.randint(0, 3) ,
+    "mdef":random.randint(0, 3) , 
+    "atk":random.randint(0, 3) , 
+    "matk":random.randint(0, 3) , 
+    "gold":0 , 
+    "weapon":"bare hands" , 
+    "armor":"commoner clothes" , 
+    "stageNum":1}
     return(playerDictionary)
 player = createPlayer()
 def stats():
