15d4e3e762ac7c2ca43a1daac606337edbfccedd
Mia <crazymimi2603@gmail.com>
2020-05-17 23:03:27 -0400

    switched around some stuff here

--- start.py
+++ start.py
@@ -38,10 +38,10 @@ def createPlayer():
     def playerQuestions():
         statDict = {"att":0}
         statDict["att"] = input("If you live in a rural area, enter 3, but if in a city area, enter 0")
-        playerPref = input("Were you more into academics or sports?")
         if(statDict["att"] == 3):
             print("You live in a rural area. Your attack increases by 1." + (playerDictionary["atk"] + 1))
         elif(statDict["att"] == 0):
+            print("You live in a city area. Physical defense increases by 1." + (playerDictionary["pdef"] + 1))
             if(playerPref == "academics"):
                 print("You were more interested in academics. Your magic attack raised by 1." + (playerDictionary["matk"] + 1))
             elif(playerPref == "sports"):
@@ -50,6 +50,13 @@ def createPlayer():
                 print("That is not a valid answer")
         else:
             print("That is not a valid input.")
+        playerPref = input("Were you more into academics or sports?")
+        if(playerPref == "academics"):
+            print("You were more interested in academics. Your magic attack raised by 1." + (playerDictionary["matk"] + 1))
+        elif(playerPref == "sports"):
+            print("You were more interested in sports. Your health increased by 2." + (playerDictionary["health"] + 2))
+        else:
+            print("That is not a valid answer")
     playerQuestions()
     return(playerDictionary)
 player = createPlayer()
