4c45e43058925150670ef6426a20cb219fb18174
David C <dlccosio@gmail.com>
2020-05-18 21:30:34 -0400

    -moved the main game code to the bottom

--- start.py
+++ start.py
@@ -254,6 +254,24 @@ def itemCreation(playerLevel:5):
     return createdItem
         
 
-enemy_generator(player["level"])   
+while True:
+    print("********************")
+    print("*     Welcome      *")
+    print("*   To Our Game!   *")
+    print("********************")
 
+    print("Type 'Start' to start!")
+    print("Type 'Load' to load your save file!")
+    playerInput = input().lower()
+    
+    if (playerInput == "start"):
+        print("started game")
+        break
 
+    elif (playerInput == "load"):
+        print("loaded save")
+        break
+    
+
+    else: 
+        print("Fricking type something right you twat")
