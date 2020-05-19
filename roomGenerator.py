95d8e58e2327ca665db5f0b8e588a81a619e9775
David C <dlccosio@gmail.com>
2020-05-18 20:14:57 -0400

    - Copied createPlayer function to utilize player dict

--- roomGenerator.py
+++ roomGenerator.py
@@ -1,1 +1,12 @@
-def 
+def createPlayer():
+    playerDictionary = {"name":input("Please input your name:") , 
+    "level":1 , 
+    "health":10 , 
+    "pdef":random.randint(0, 3) ,
+    "mdef":random.randint(0, 3) , 
+    "atk":random.randint(0, 3) , 
+    "matk":random.randint(0, 3) , 
+    "gold":0 , 
+    "weapon":"bare hands" , 
+    "armor":"commoner clothes" , 
+    "stageNum":1}
