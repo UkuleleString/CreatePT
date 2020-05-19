a01d8a5f9549f8f3f9096a7ac5a7327a2d8a7080
David C <dlccosio@gmail.com>
2020-05-18 21:31:05 -0400

    - added terminal clear function

--- start.py
+++ start.py
@@ -1,26 +1,13 @@
 import pickle
 import random
+from os import system, name 
+from time import sleep 
 
-while True:
-    print("********************")
-    print("*     Welcome      *")
-    print("*   To Our Game!   *")
-    print("********************")
-
-    print("Type 'Start' to start!")
-    print("Type 'Load' to load your save file!")
-    playerInput = input().lower()
-    
-    if (playerInput == "start"):
-        print("started game")
-        break
-
-    elif (playerInput == "load"):
-        print("loaded save")
-        break
-    
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
     else: 
-        print("Fricking type something right you twat")
+        _ = system('clear') 
 
 def createPlayer():
     playerDictionary = {"name":input("Please input your name:") , 
